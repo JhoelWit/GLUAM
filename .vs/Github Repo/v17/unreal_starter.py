3d1fda99eac14d7e6d077891f1a66b8ba56182dd
JhoelWit <Jhoelwit@buffalo.edu>
2022-04-21 21:26:49 -0400

    Code to start unreal env's in parallel

---
+++ .vs/Github Repo/v17/unreal_starter.py
@@ -0,0 +1,10 @@
+import subprocess
+
+pathtoexe = r'D:\p00hb\Documents\MAE 600\UAM1\UAM1 4.27\WindowsNoEditor\UAM1.exe'
+commands = [[pathtoexe,'-RenderOffscreen'], [pathtoexe,'-RenderOffscreen']] # add as many as you want for more environments in parallel
+procs = [subprocess.Popen(i) for i in commands]
+for p in procs:
+    p.wait() #Will start all and wait for the first one to close until all are closed.
+
+
+
